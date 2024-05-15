7cb4120dc44e94a50607f09189aabe84fa683916
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2024-05-16 00:12:20 +0530

    add ALLOWED_ORIGINS for server

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(helmet());
 app.use(bodyParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: JSON.parse(process.env.ALLOWED_ORIGINS),
     credentials: true,
   })
 );
